69679712aa57afb576bc2cdbdef86826b1e049e5
shiyun.wxm <shiyun.wxm@taobao.com>
2017-05-01 00:03:31 +0800

    update spark

--- spark-2.1.0-hadoop-2.7/bin/start-spark.sh
+++ spark-2.1.0-hadoop-2.7/bin/start-spark.sh
@@ -3,3 +3,6 @@
 source ${SPARK_HOME}/sbin/start-master.sh -h spark -p 7077 --webui-port 8089
 
 source ${SPARK_HOME}/sbin/start-slave.sh spark://spark:7077
+
+# test
+${SPARK_HOME}/bin/spark-submit --master spark://spark:7077 --class "org.apache.spark.examples.SparkPi" ${SPARK_HOME}/examples/jars/spark-examples_2.11-2.1.0.jar 
